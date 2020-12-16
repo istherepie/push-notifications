75092fffc9121917de8df22d488a7b150be7d9a5
Steffen Park <dev@istherepie.com>
2020-12-16 18:17:41 +0100

    Add first draft of event broker

---
+++ eventbroker/eventbroker.go
@@ -0,0 +1,105 @@
+package event
+
+import (
+	"sync"
+	"time"
+)
+
+type Subscription struct {
+	listener chan string
+	quit     chan struct{}
+}
+
+func (s *Subscription) Next() <-chan string {
+	return s.listener
+}
+
+func (s *Subscription) Close() {
+	close(s.quit)
+}
+
+type Broker struct {
+	subscriptions map[*Subscription]struct{}
+	mtx           sync.RWMutex
+}
+
+func (b *Broker) Init() {
+	b.subscriptions = make(map[*Subscription]struct{})
+}
+
+func (b *Broker) addSub(subscription *Subscription) {
+	b.mtx.Lock()
+	defer b.mtx.Unlock()
+
+	var empty struct{}
+	b.subscriptions[subscription] = empty
+}
+
+func (b *Broker) remSub(subscription *Subscription) {
+	b.mtx.Lock()
+	defer b.mtx.Unlock()
+	close(subscription.listener)
+	delete(b.subscriptions, subscription)
+}
+
+func (b *Broker) waitForClose(sub *Subscription) {
+	<-sub.quit
+	b.remSub(sub)
+}
+
+func (b *Broker) Subscribe() *Subscription {
+
+	subscription := &Subscription{
+		listener: make(chan string, 1),
+		quit:     make(chan struct{}),
+	}
+
+	b.addSub(subscription)
+
+	// Remove when closed
+	go b.waitForClose(subscription)
+
+	return subscription
+}
+
+func (b *Broker) GetSubs() []*Subscription {
+	b.mtx.RLock()
+	defer b.mtx.RUnlock()
+
+	var subscribers []*Subscription
+
+	for subscriber := range b.subscriptions {
+		subscribers = append(subscribers, subscriber)
+	}
+
+	return subscribers
+}
+
+func (b *Broker) CountSubs() int {
+	b.mtx.RLock()
+	defer b.mtx.RUnlock()
+	return len(b.subscriptions)
+}
+
+func (b *Broker) Publish(message string) {
+
+	if b.CountSubs() == 0 {
+		return
+	}
+
+	broadcast := func(sub *Subscription, message string) {
+		select {
+		case sub.listener <- message:
+			return
+
+		// TODO: The timeout should be configurable
+		case <-time.After(300 * time.Millisecond):
+			return
+		}
+	}
+
+	for _, subscription := range b.GetSubs() {
+		go broadcast(subscription, message)
+	}
+
+}
---
+++ eventbroker/eventbroker_test.go
@@ -0,0 +1,86 @@
+package event
+
+import (
+	"testing"
+)
+
+func TestSubscriberCount(t *testing.T) {
+	broker := Broker{}
+	broker.Init()
+
+	broker.Subscribe()
+
+	// Count subs
+	result := broker.CountSubs()
+
+	// Expecting 1 sub
+	expected := 1
+
+	if result != expected {
+		t.Errorf("Incorrect amount of subs - result: %d (Should be %d)", result, expected)
+	}
+}
+
+func TestAddAndRemoveSubs(t *testing.T) {
+	broker := Broker{}
+	broker.Init()
+
+	// Create 4 subs
+	sub1 := broker.Subscribe()
+	sub2 := broker.Subscribe()
+	sub3 := broker.Subscribe()
+	sub4 := broker.Subscribe()
+
+	// Close subs
+	sub1.Close()
+	sub2.Close()
+	sub3.Close()
+	sub4.Close()
+
+	// Wait for all to close
+	<-sub1.Next()
+	<-sub2.Next()
+	<-sub3.Next()
+	<-sub4.Next()
+
+	// Count subs
+	result := broker.CountSubs()
+
+	// Expecting 1 sub
+	expected := 0
+
+	if result != expected {
+		t.Errorf("Incorrect amount of subs - result: %d (Should be %d)", result, expected)
+	}
+}
+
+func TestEventNotification(t *testing.T) {
+
+	broker := Broker{}
+	broker.Init()
+
+	sub := broker.Subscribe()
+
+	// Fixtures
+	testmessages := []string{
+		"test message 1",
+		"test message 2",
+		"test message 3",
+		"test message 4",
+		"test message 5",
+	}
+
+	// Test all 5 messages
+	for i := 0; i < 5; i++ {
+		broker.Publish(testmessages[i])
+
+		// Get message
+		result := <-sub.Next()
+
+		if result != testmessages[i] {
+			t.Errorf("Message I/O error, expected: %v , got: %v", result, testmessages[i])
+		}
+
+	}
+
+}

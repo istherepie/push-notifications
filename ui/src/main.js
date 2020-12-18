97409c537738885f2b41791e7f29c9c1405d2545
Steffen Park <dev@istherepie.com>
2020-12-18 16:56:33 +0100

    Added frontend prototype

--- ui/src/main.js
+++ ui/src/main.js
@@ -13,15 +13,73 @@ import '$base/style.css'
 class App {
 	constructor(elementID) {
 		this.root = document.getElementById(elementID)
+		this.input = document.getElementById("notification-input")
+		this.area = document.getElementById("notification-area")
+		this.socket = new EventSource("/api/notifications")
 	}
 
-	Hello() {
-		let title = document.createElement("h1")
-		title.innerHTML = "Hello World"
-		this.root.append(title)
+	createNotification(message) {
+		// Create notification container
+		let notification = document.createElement("div")
+		notification.classList = ["notification"]
+
+		// Create title
+		let title = document.createElement("h4")
+		title.innerHTML = "Notification"
+		title.classList = ["notification-title"]
+
+		// Create body
+		let body = document.createElement("div")
+		body.innerHTML = message
+
+		// Append title and body
+		notification.append(title, body)
+		return notification
+	}
+
+	notify(message) {
+		let notification = this.createNotification(message)
+		this.area.append(notification)
+	}
+
+	fire(event) {
+		event.preventDefault()
+		console.log(event)
+
+		this.request()
+	}
+
+	request() {
+		var req = new XMLHttpRequest();
+
+		req.addEventListener("load", event => {
+			console.log(event)
+		});
+
+		req.onreadystatechange = event => {
+			console.log(event)
+		}
+
+		let payload = {
+			message: this.input.value
+		}
+
+		let postData = JSON.stringify(payload)
+
+		req.open("POST", "/api/message");
+		req.send(postData);
+	}
+
+	Run() {
+		document.body.append(this.area)
+
+		this.socket.onmessage = (event) => {this.notify(event.data)}
+
+		let button = document.getElementById("submit")
+		button.addEventListener("click", event => this.fire(event));
 	}
 }
 
-window.onload = () => {
-	new App("root").Hello()
-}
+// Init the app!
+const app = new App("root")
+window.onload = () => { app.Run() }

61ffda2017316b0d18c5daf8b8c702a224e15be9
Steffen Park <dev@istherepie.com>
2020-12-26 02:31:51 +0100

    Add application flags

--- cmd/notification-server/notification-server.go
+++ cmd/notification-server/notification-server.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net"
 	"net/http"
+	"os"
 
 	"github.com/istherepie/push-notifications/cmd/notification-server/webserver"
 	"github.com/istherepie/push-notifications/eventbroker"
@@ -11,7 +14,26 @@ import (
 )
 
 func main() {
-	listener, err := net.Listen("tcp", "localhost:8080")
+	hostname := flag.String("host", "localhost", "Set the host address of the service.")
+	port := flag.Int("port", 8080, "Set the tcp port of the service.")
+	static := flag.String("static", "", "Specify a directory to serve static files from.")
+	flag.Parse()
+
+	var webDir string
+
+	if *static != "" {
+		path := fmt.Sprintf("%v/index.html", *static)
+		_, err := os.Stat(path)
+
+		if os.IsNotExist(err) {
+			log.Printf("No index.html found in path: %v", *static)
+		}
+
+		webDir = *static
+	}
+
+	addr := fmt.Sprintf("%v:%d", *hostname, *port)
+	listener, err := net.Listen("tcp", addr)
 
 	if err != nil {
 		log.Fatal(err)
@@ -49,7 +71,7 @@ func main() {
 
 	go broker.Run()
 
-	mux := webserver.Mux(broker, counter)
+	mux := webserver.Mux(webDir, broker, counter)
 
 	serveErr := http.Serve(listener, mux)
 
